1a50b5fb610b08fc7c9469be1976cd5aca748a12
Vitalii Satanovskyi <satanovskiy.vitaliy95@gmail.com>
2020-11-15 20:58:45 +0200

    Basic app config NodeJS + TS

---
+++ src/routers/MasterRouter.ts
@@ -0,0 +1,24 @@
+import { Router } from 'express';
+import ThemeARouter from './themeA/ThemeARouter';
+import ThemeBRouter from './themeB/ThemeBRouter';
+
+class MasterRouter {
+  private _router = Router();
+  private _subrouterA = ThemeARouter;
+  private _subrouterB = ThemeBRouter;
+
+  get router() {
+    return this._router;
+  }
+
+  constructor() {
+    this._configure();
+  }
+
+  private _configure() {
+    this._router.use('/themeA', this._subrouterA);
+    this._router.use('/themeB', this._subrouterB);
+  }
+}
+
+export = new MasterRouter().router;
---
+++ src/routers/themeA/ThemeARouter.ts
@@ -0,0 +1,28 @@
+import { NextFunction, Request, Response, Router } from 'express';
+import ThemeAController from '../../controllers/ThemeAController';
+
+class ThemeARouter {
+  private _router = Router();
+  private _controller = ThemeAController;
+
+  get router() {
+    return this._router;
+  }
+
+  constructor() {
+    this._configure();
+  }
+
+  private _configure() {
+    this._router.get('/', (req: Request, res: Response, next: NextFunction) => {
+      try {
+        const result = this._controller.defaultMethod();
+      res.status(200).json(result);
+      } catch(error) {
+        next(error);
+      }
+    });
+  }
+}
+
+export = new ThemeARouter().router;
